0b82db58af48ddcc4c36df71b946ab433f143b88
James Britton <brittonjp@gmail.com>
2019-03-19 13:24:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,9 @@
 def square_array(array)
   #new_array = []
   #array.each {|n| new_array.push(n**2)}
+  new_array = []
+  array.each do |array_element|
+    new_array << array_element**2
+  end
+  new_array
 end
